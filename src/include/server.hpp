8ce1d5f2a1c73e9efc65cc714be4ccc47e8bfcd1
Oleksii Myronenko <idokka@gmail.com>
2016-02-23 22:16:19 +0200

    split server socket class

--- src/include/server.hpp
+++ src/include/server.hpp
@@ -1,14 +1,6 @@
 #pragma once
-#include <cerrno>
-#include <thread>
 #include <functional>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include "logger.hpp"
-#include "client.hpp"
 #include "sock.hpp"
-#include "client.hpp"
 
 namespace ukrnet
 {
@@ -25,115 +17,22 @@ namespace ukrnet
 
 	public:
 		// default constructor
-		Server()
-			: _port(0)
-			, _is_opened(false)
-		{
-			// none of init
-		}
-		
+		Server();
 		// default destructor; closes socket if needed
-		~Server()
-		{
-			Close();
-		}
+		~Server();
 
 		// open socket
 		// returns true, if socket has successfuly opened, otherwise false
-		bool Open()
-		{
-			// check if socket already is opened
-			if (_is_opened)
-				return true;
-			// check is socket port is setted
-			if (_port == 0)
-			{
-				logger().err("server", "port not specified");
-				return false;
-			}
-
-			// try create socket
-			_sock.desc = socket(AF_INET, SOCK_STREAM, 0);
-			if (_sock.desc < 0)
-			{
-				logger().err("server", "cannot open descriptor", errno);
-				return false;
-			}
-
-			// fill addr struct
-			_sock.addr.sin_family = AF_INET;
-			_sock.addr.sin_addr.s_addr = INADDR_ANY;
-			_sock.addr.sin_port = htons(_port);
-
-			// try bind socket
-			int error = bind(_sock.desc, (const struct sockaddr *) &_sock.addr, _sock.addr_len);
-			if (error < 0)
-			{
-				logger().err("server", "cannot bind socket", errno);
-				return false;
-			}
-
-			// try listen socket
-			error = listen(_sock.desc, 5);
-			if (error < 0)
-			{
-				logger().err("server", "cannot listen socket", errno);
-				return false;
-			}
-
-			// socket opening successful
-			logger().nfo("server", "socket opened", _port);
-			_is_opened = true;
-			return true;
-		}
-
+		bool Open();
 		// closes socket
 		// returns true, if socket has successfuly closed, otherwise false
-		bool Close()
-		{
-			// check if socket is already closed
-			if (_is_opened == false)
-				return true;
-
-			// close socket
-			int error = close(_sock.desc);
-			if (error < 0)
-			{
-				logger().err("server", "cannot close socket", errno);
-				return false;
-			}
-
-			// socket closing successful
-			_is_opened = false;
-			return true;
-		}
-
-		// main accpet loop; creates thread on every incloming connection, call client functor
-		bool Accept()
-		{
-			if (_is_opened == false)
-			{
-				logger().err("server", "socket not opened");
-				return false;
-			}
-			while (true)
-			{
-				Sock client;
-				logger().nfo("server", "wait for connections");
-				client.desc = accept(_sock.desc, (struct sockaddr *) &client.addr, &client.addr_len);
-				logger().nfo("server", "new connection accepted");
-				// start new thread to work with client socket stream
-				auto do_client_execute = std::bind(& Server::DoClientExecute, this, std::placeholders::_1);
-				std::thread(do_client_execute, client).detach();
-			}
-		}
+		bool Close();
+		// main accept loop; creates thread on every incloming connection, call client functor
+		bool Accept();
 
 	private:
-		void DoClientExecute(Sock sock)
-		{
-			Client client(sock);
-			_client_func(client);
-		}
+		// main client function
+		void DoClientExecute(Sock sock);
 
 	public:
 		// get socket port
