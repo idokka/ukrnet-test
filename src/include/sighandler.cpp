2e368bbf158addbc247f18f94e8fcb25030e673f
Oleksii Myronenko <idokka@gmail.com>
2016-03-14 19:59:54 +0200

    fix sighandler implemetation

--- src/include/sighandler.cpp
+++ src/include/sighandler.cpp
@@ -8,25 +8,25 @@ SigHandler::onSignal SigHandler::_on_sigusr1;
 SigHandler::onSignal SigHandler::_on_sigusr2;
 
 // get current SIGUSR1 handler
-onSignal on_sigusr1()
+auto SigHandler::on_sigusr1() -> onSignal
 {
 	return _on_sigusr1;
 }
 
 // set SIGUSR1 handler
-void set_on_sigusr1(onSignal value)
+void SigHandler::set_on_sigusr1(onSignal value)
 {
 	SetEvent(SIGUSR1, value, _on_sigusr1);
 }
 
 // get current SIGUSR2 handler
-onSignal on_sigusr2()
+auto SigHandler::on_sigusr2() -> onSignal
 {
 	return _on_sigusr2;
 }
 
 // set SIGUSR2 handler
-void set_on_sigusr2(onSignal value)
+void SigHandler::set_on_sigusr2(onSignal value)
 {
 	SetEvent(SIGUSR2, value, _on_sigusr2);
 }
